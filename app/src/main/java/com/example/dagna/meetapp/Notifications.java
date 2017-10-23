3babf475f4d8b71ed7fe4778a14e397393c2f360
Daniel Pires <danielpires96@gmail.com>
2017-10-23 20:32:29 +0100

    notification activity list view

--- app/src/main/java/com/example/dagna/meetapp/Notifications.java
+++ app/src/main/java/com/example/dagna/meetapp/Notifications.java
@@ -6,8 +6,18 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Notifications extends AppCompatActivity {
+
+    //LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
+    ArrayList<String> listItems=new ArrayList<String>();
+
+    //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +27,19 @@ public class Notifications extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ListView list = (ListView) findViewById(R.id.notifications_list);
+
+
+        for (int i=0; i<20; i++){
+            listItems.add("notification " + String.valueOf(i));
+        }
+
+        adapter=new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                listItems);
+
+        list.setAdapter(adapter);
     }
 
 }
