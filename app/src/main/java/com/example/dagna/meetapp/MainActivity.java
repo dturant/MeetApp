8caef7acd0f4f34c0ff49539a00e7b34de2ea8ff
Daniel Pires <danielpires96@gmail.com>
2017-10-26 14:41:07 +0100

    on back pressed fix

--- app/src/main/java/com/example/dagna/meetapp/MainActivity.java
+++ app/src/main/java/com/example/dagna/meetapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.dagna.meetapp;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -11,6 +14,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -66,10 +70,33 @@ public class MainActivity extends AppCompatActivity
 
     }
 
+
     @Override
-    public void onBackPressed(){
-        finish();
+    public void onBackPressed() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setMessage("Do you want to Exit?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //if user pressed "yes", then he is allowed to exit from application
+                finishAffinity();
+            }
+        });
+
+        builder.setNegativeButton("No",new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //if user select "No", just cancel this dialog and continue with app
+                dialog.cancel();
+            }
+        });
+        AlertDialog alert=builder.create();
+        alert.show();
     }
+
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -176,6 +203,9 @@ public class MainActivity extends AppCompatActivity
             startActivity(intent);
 
         } else if (id == R.id.nav_logout) {
+
+            Intent intent = new Intent(this, Login.class);
+            startActivity(intent);
 
         }
 
