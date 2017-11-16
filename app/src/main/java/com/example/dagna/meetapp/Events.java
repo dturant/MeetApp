4d8ce130e932dcb5f377cecda4abac0b3822d419
Daniel Pires <danielpires96@gmail.com>
2017-11-16 16:42:33 +0000

    List of events

--- app/src/main/java/com/example/dagna/meetapp/Events.java
+++ app/src/main/java/com/example/dagna/meetapp/Events.java
@@ -1,11 +1,13 @@
 package com.example.dagna.meetapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridLayout;
 import android.widget.GridView;
@@ -18,6 +20,9 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import java.util.ArrayList;
 
 public class Events extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.example.dagna.meetapp.MESSAGE";
+
     //LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
     ArrayList<String> listItems=new ArrayList<String>();
 
@@ -48,7 +53,26 @@ public class Events extends AppCompatActivity {
 
         list.setAdapter(adapter);
 
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+
+                Intent intent = new Intent(Events.this, Event.class);
+                String message = listItems.get(position);
+                intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
+
+            }
+
+        });
+
+
+
 
     }
+
+
 
 }
