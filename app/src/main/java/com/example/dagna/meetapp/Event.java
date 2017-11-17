66d05fc1b9150ef6c406cddbcfea38e42d6b267b
dturant <dagna.turant@gmail.com>
2017-11-17 15:08:25 +0000

    EventCreate improved; Event layout
    EventCreate - adding photos, choosing category from a dropdown, date and time picker; glyphicons added

--- app/src/main/java/com/example/dagna/meetapp/Event.java
+++ app/src/main/java/com/example/dagna/meetapp/Event.java
@@ -7,6 +7,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TabHost;
 import android.widget.TextView;
 
 public class Event extends AppCompatActivity {
@@ -18,13 +19,27 @@ public class Event extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Intent intent = getIntent();
         String title = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
-        TextView tv = (TextView) findViewById(R.id.marker_title);
-        tv.setText(title);
+        TabHost host = (TabHost)findViewById(R.id.groups_tab);
+        host.setup();
+
+        TabHost.TabSpec spec1 = host.newTabSpec("Info");
+        spec1.setContent(R.id.tab1);
+        spec1.setIndicator("Info");
+        host.addTab(spec1);
+
+        TabHost.TabSpec spec2 = host.newTabSpec("Participants");
+        spec2.setContent(R.id.tab2);
+        spec2.setIndicator("Participants");
+        host.addTab(spec2);
+
+
+       // TextView tv = (TextView) findViewById(R.id.marker_title);
+       // tv.setText(title);
     }
 
 }
--- app/src/main/java/com/example/dagna/meetapp/EventCreate.java
+++ app/src/main/java/com/example/dagna/meetapp/EventCreate.java
@@ -1,23 +1,41 @@
 package com.example.dagna.meetapp;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.Calendar;
+
 public class EventCreate extends AppCompatActivity {
 
     public LatLng location;
     public Intent intent;
+    public ImageView eventPhoto;
+    public EditText date,time;
+    static final int DATE_DIALOG_ID = 0;
+    static final int TIME_DIALOG_ID = 0;
+    private int mYear,mMonth,mDay,mHour,mMinutes;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +45,16 @@ public class EventCreate extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        eventPhoto = (ImageView) findViewById(R.id.event_photo);
+        date=(EditText) findViewById(R.id.event_date);
+        time=(EditText) findViewById(R.id.event_time);
+
+        spinner = (Spinner) findViewById(R.id.event_category);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.categories, R.layout.spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
         intent = getIntent();
         String[] locationString = intent.getStringExtra(MainActivity.EXTRA_MESSAGE).split(",");
@@ -50,6 +78,73 @@ public class EventCreate extends AppCompatActivity {
         super.finish();
 
     }
+
+    protected void addPhoto(View view){
+        Intent pickPhoto = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(pickPhoto , 1);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        switch(requestCode) {
+            case 0:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    eventPhoto.setImageURI(selectedImage);
+                }
+
+                break;
+            case 1:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    eventPhoto.setImageURI(selectedImage);
+                }
+                break;
+        }
+    }
+
+    protected void pickDate(View view){
+        Calendar mcurrentDate = Calendar.getInstance();
+        int mYear = mcurrentDate.get(Calendar.YEAR);
+        int mMonth = mcurrentDate.get(Calendar.MONTH);
+        int mDay = mcurrentDate.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog mDatePicker;
+        mDatePicker = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            public void onDateSet(DatePicker datepicker, int selectedyear, int selectedmonth, int selectedday) {
+                // TODO Auto-generated method stub
+                    /*      Your code   to get date and time    */
+                selectedmonth = selectedmonth + 1;
+                date.setText("" + selectedday + "/" + selectedmonth + "/" + selectedyear);
+            }
+        }, mYear, mMonth, mDay);
+        mDatePicker.setTitle("Select Date");
+        mDatePicker.show();
+    }
+
+
+    protected void pickTime(View view){
+        Calendar c=Calendar.getInstance();
+        mYear=c.get(Calendar.YEAR);
+        mMonth=c.get(Calendar.MONTH);
+        mDay=c.get(Calendar.DAY_OF_MONTH);
+
+        Calendar mcurrentTime = Calendar.getInstance();
+        int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+        int minute = mcurrentTime.get(Calendar.MINUTE);
+        TimePickerDialog mTimePicker;
+        mTimePicker = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int selectedHour, int selectedMinute) {
+                time.setText( selectedHour + ":" + selectedMinute);
+
+            }
+        }, hour, minute, true);//Yes 24 hour time
+        mTimePicker.setTitle("Select Time");
+        mTimePicker.show();
+    }
+
 
 
 
