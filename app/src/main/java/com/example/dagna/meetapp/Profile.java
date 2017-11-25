c7a75d9df93c994e61ebd3a4f9f9b28bee2d4604
Daniel Pires <danielpires96@gmail.com>
2017-11-25 14:22:44 +0000

    QRCode and QRCode reader done TODO: action after read QRCode

--- app/src/main/java/com/example/dagna/meetapp/Profile.java
+++ app/src/main/java/com/example/dagna/meetapp/Profile.java
@@ -2,9 +2,15 @@ package com.example.dagna.meetapp;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -12,15 +18,20 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TabHost;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.zxing.Result;
+
+import net.glxn.qrgen.android.QRCode;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -28,7 +39,11 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 
-public class Profile extends AppCompatActivity {
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+import static android.R.id.content;
+
+public class Profile extends AppCompatActivity implements ZXingScannerView.ResultHandler{
 
     public String userID;
     private DatabaseReference mFirebaseDatabaseUsers;
@@ -44,6 +59,8 @@ public class Profile extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.example.dagna.meetapp.MESSAGE";
 
     public TextView name, email;
+    private ZXingScannerView mScannerView;
+    private int PERMISSION_REQUEST_CAMERA = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +70,23 @@ public class Profile extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+
+                if (!haveCameraPermission()){
+                    ActivityCompat.requestPermissions(Profile.this,new String[]{android.Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
+                }
+                mScannerView = new ZXingScannerView(Profile.this);   // Programmatically initialize the scanner view<br />
+                setContentView(mScannerView);
+                startCamera();// Start camera<br />
+            }
+        });
 
         TabHost host = (TabHost)findViewById(R.id.profile_tab);
         host.setup();
@@ -86,11 +120,18 @@ public class Profile extends AppCompatActivity {
         email = (TextView) findViewById(R.id.email);
 
         mFirebaseDatabaseUsers.addListenerForSingleValueEvent(new ValueEventListener() {
+
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
 
                 name.setText(dataSnapshot.child("nome").getValue().toString());
                 email.setText(dataSnapshot.child("email").getValue().toString());
+
+                Bitmap myBitmap = QRCode.from(userID).bitmap();
+                ImageView myImage = (ImageView) findViewById(R.id.QRCode);
+                myImage.setImageBitmap(myBitmap);
+
+
 
             }
 
@@ -99,6 +140,8 @@ public class Profile extends AppCompatActivity {
 
             }
         });
+
+
 
         mFirebaseDatabaseMarkers = mFirebaseInstance.getReference("markers");
         mFirebaseDatabaseMarkers.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -166,4 +209,57 @@ public class Profile extends AppCompatActivity {
 
     }
 
+
+
+    private boolean haveCameraPermission()
+    {
+        if (Build.VERSION.SDK_INT < 23)
+            return true;
+        return checkSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults){
+        // This is because the dialog was cancelled when we recreated the activity.
+        if (permissions.length == 0 || grantResults.length == 0)
+            return;
+
+        switch (requestCode)
+        {
+            case 1:
+            {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    startCamera();
+                }
+                else
+                {
+                    finish();
+                }
+            }
+            break;
+        }
+    }
+
+
+    public void startCamera()
+    {
+        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        mScannerView.startCamera();          // Start camera on resume
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        Log.i("QRCODE",result.getText());
+        mScannerView.stopCamera();
+        setContentView(R.layout.activity_profile);
+    }
+
+
+
+    @Override
+    public void onPause() {
+            super.onPause();
+            mScannerView.stopCamera();   // Stop camera on pause<br />
+    }
 }
